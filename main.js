ddd0d862073e8ae8cff216f78e2c0e046009a549
Karan10969 <111994633+Karan10969@users.noreply.github.com>
2024-09-04 22:32:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+const contactInfo = document.querySelector(".contact-info")
+const closeContactButton = document.querySelector(".close-contact-button")
+const contactMeButton = document.querySelector(".contact-me-button")
+const aboutDesDiv = document.querySelector(".about-desc")
+
+//------nav buttons-----
+
+const onNavHomeClick = () => {
+    document.querySelector("#home-container").scrollIntoView({behavior: "smooth"});
+}
+const onNavAboutClick = () => {
+    document.querySelector("#about-container").scrollIntoView({behavior: "smooth"});
+}
+const onNavProjectClick = () => {
+    document.querySelector("#project-container").scrollIntoView({behavior: "smooth"});
+}
+
+//-----------------------------
+
+const contactButton = () => {
+    contactInfo.style.display = "block";
+    contactMeButton.style.display = "none";
+    closeContactButton.style.display = "block";
+}
+
+const closeContactFn = () =>{
+    contactInfo.style.display = "none";
+    contactMeButton.style.display = "block";
+    closeContactButton.style.display = "none";
+}
+
+//--------clipboard copy function
+const copyEmail = () => {
+    navigator.clipboard.writeText("karanyash1111@gmail.com");
+    alert("copied to clipboard");
+}
+
+const copyNumber = () => {
+    navigator.clipboard.writeText("7838898851");
+    alert("copied to clipboard");
+}
+
+// ------------- closing and opeing of Qualification--------- 
+const onQualificationClick = () => {
+    document.querySelector("#qualificationDesc").style.display = "block"
+}
+const onCloseQualiClick = () => {
+    document.querySelector("#qualificationDesc").style.display = "none"
+}
+
+// ------------- closing and opeing of Skills--------- 
+const onSkillsClick = () => {
+    document.querySelector("#skillsDesc").style.display = "block"
+}
+const onCloseSkillClick = () => {
+    document.querySelector("#skillsDesc").style.display = "none"
+}
+
+// ------------- closing and opeing of Work--------- 
+const onWorkClick = () => {
+    document.querySelector("#workDesc").style.display = "block"
+}
+const onCloseWork = () => {
+    document.querySelector("#workDesc").style.display = "none"
+}
+
+
